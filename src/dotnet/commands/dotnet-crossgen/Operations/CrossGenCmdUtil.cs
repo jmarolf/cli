09d0635c18dbea5ce865e4866e963d80981e8f83
Peter Hsu <shhsu@microsoft.com>
2016-09-22 16:07:21 -0700

    filter standard output

--- src/dotnet/commands/dotnet-crossgen/Operations/CrossGenCmdUtil.cs
+++ src/dotnet/commands/dotnet-crossgen/Operations/CrossGenCmdUtil.cs
@@ -68,6 +68,7 @@ namespace Microsoft.DotNet.Tools.CrossGen.Operations
                 .WorkingDirectory(appPath)
                 // disable partial ngen
                 .EnvironmentVariable("COMPlus_PartialNGen", "0")
+                .CaptureStdOut()
                 .CaptureStdErr();
 
             var result = cmd.Execute();
@@ -87,6 +88,7 @@ namespace Microsoft.DotNet.Tools.CrossGen.Operations
                 var pdbArgs = string.Join(" ", GetArgs(appPath, assemblyLocation, module, platformAssembliesPaths, outputDirectory, true));
                 var pdbCmd = Command.Create(new CommandSpec(_crossgenPath, pdbArgs, CommandResolutionStrategy.None))
                                 .WorkingDirectory(appPath)
+                                .CaptureStdOut()
                                 .CaptureStdErr();
 
                 var pdbResult = pdbCmd.Execute();
